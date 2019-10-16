4a5c2c144230f2a14eadbc2e2c18de831d35db30
Euripedes Cabral <euripedescabral@dominio.local>
2019-10-16 06:55:20 -0300

    First commit

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '420f985c61a182e8b240a09e738d441a',
+  expiresIn: '7d',
+};
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+import { Router } from 'express';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
+
+import auth from './app/middlewares/auth';
+
+const routes = new Router();
+
+routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
+routes.use(auth);
+routes.put('/users', UserController.update);
+
+export default routes;
